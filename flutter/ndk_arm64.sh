ae949d19be8748de977d2e46782c9e8b8e5e064b
ak <287971051@qq.com>
2026-06-15 09:25:49 +0800

    回滚到只编译 64位APK

--- flutter/ndk_arm64.sh
+++ flutter/ndk_arm64.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-cargo ndk --platform 21 --target aarch64-linux-android build --release --features flutter,hwcodec
+export CARGO_INCREMENTAL=0
+export CARGO_PROFILE_RELEASE_CODEGEN_UNITS=1
+export CARGO_PROFILE_RELEASE_LTO=fat
+cargo ndk --platform 21 --target aarch64-linux-android build --release --features flutter,hwcodec -j 4
